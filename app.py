de9629908c357a04729e57d13b1c31ee36995485
mazharul.miraz <mazharul.miraz@outlook.com>
2019-08-11 22:31:22 +0530

    add URL_builder

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import render_template,  url_for
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def appview():
+    return render_template('view.html')
+
 
 
 if __name__ == '__main__':
