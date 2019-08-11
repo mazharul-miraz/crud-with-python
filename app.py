8f002b004e4ec09e78b8f750c7b877ba2e79ec83
mazharul.miraz <mazharul.miraz@outlook.com>
2019-08-12 00:40:04 +0530

    link fixed

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ def appview():
     return render_template('view.html')
 
 
+@app.route('/insert')
+def insert():
+    return render_template('insert.html')
+
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+
+
+
+
 
 if __name__ == '__main__':
    app.run()
