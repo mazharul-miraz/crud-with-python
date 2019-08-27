2058703d8e88756737743370531017b439ddfea6
mazharul.miraz <mazharul.miraz@outlook.com>
2019-08-28 02:54:22 +0530

    insert path error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('localhost', 27017)
 db = client.pycrud
-
+print(db)
 
 app = Flask(__name__)
 
@@ -16,24 +16,72 @@ def appview():
     return render_template('view.html')
 
 
-@app.route('/insert', methods =['POST','GET'])
-def insert():
-    if request.method == 'POST':
-        return render_template('insert.html')
-    else:
-        return "this is a get request"
-    
-
-
-@app.route('/update')
-def update():
-    return render_template('update.html')
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
+#============ INSERT START #============# 
+
+@app.route('/insert', methods =['POST','GET'])
+def insert():
+    if request.method == 'POST':
+        return InsertInfo(request)
+    else:
+         return render_template('insert.html')
+
+def InsertInfo(request):
+    UserName = request.form['username']
+    UserEmail = request.form['email']
+    UserAddress = request.form['address']
+    UserPhone = request.form['phone']
+
+
+    ExistingUser =  db.user.find_one({
+            "email": UserEmail
+        })
+
+    if ExistingUser != None:
+        print(ExistingUser)
+        return 'This email is already registered'
+    else:
+        db.user.insert_one({
+        "name": UserName,
+        "email": UserEmail,
+        "address": UserAddress,
+        "phone": UserPhone,
+    })
+    return "Registration successfull"
+    # return  render_template('userdash.html')
+    # return redirect(url_for('appview' ))
+
+
+
+#============ INSERT END #============# 
+
+
+
+
+
+#============ UPDATE START #============# 
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+#============ UPDATE END #============#
+
+
+
+#============ DELETE START #============# 
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+
+#============ DELETE END #============#
 
 
 if __name__ == '__main__':
